16b3da13537911c8120e21ed267169cd79cb2a28
ElliottDylan60 <elliottdylan60@gmail.com>
2022-11-16 18:24:52 -0800

    Now generates random expressions

--- CalculatorTests/Form1.cs
+++ CalculatorTests/Form1.cs
@@ -14,15 +14,18 @@ namespace CalculatorTests
     public partial class Form1 : Form
     {
         // Global Variables
-        Graph graph;
-        Vertex Arithmetic = new Vertex("Arithmetic");
-        Vertex Digit = new Vertex("Digit");
+        Graph graph = new Graph();
+        Vertex Add = new Vertex("+");
+        Vertex Subtract = new Vertex("-");
+        Vertex Multiply = new Vertex("*");
+        Vertex Divide = new Vertex("/");
+        Vertex Int = new Vertex("Int");
+        Vertex Double = new Vertex("Double");
         Vertex UnaryMinus = new Vertex("Unary Minus");
         Vertex Functions = new Vertex("Functions");
         Vertex OpenParenthesis = new Vertex("(");
         Vertex CloseParenthesis = new Vertex(")");
         Vertex Empty = new Vertex("Empty");
-        Vertex Decimal = new Vertex("Decimal");
         Vertex Done = new Vertex("Done");
 
         public Form1()
@@ -33,69 +36,174 @@ namespace CalculatorTests
         private void Form1_Load(object sender, EventArgs e)
         {
             InitializeGraph();
-            Console.WriteLine(graph);
+            
+            evaluateRandom(graph.randomTraversal(Empty));
         }
         private void InitializeGraph() {
             // Create Graph
-            graph = new Graph();
             #region AddingNodes
-            
-
-            graph.addVertex(Arithmetic);
-            graph.addVertex(Digit);
+            graph.addVertex(Int);
+            graph.addVertex(Double);
+            graph.addVertex(Add);
+            graph.addVertex(Subtract);
+            graph.addVertex(Multiply);
+            graph.addVertex(Divide);
             graph.addVertex(UnaryMinus);
             graph.addVertex(Functions);
             graph.addVertex(OpenParenthesis);
             graph.addVertex(CloseParenthesis);
             graph.addVertex(Empty);
-            graph.addVertex(Decimal);
             graph.addVertex(Done);
             #endregion
+
             #region AddingEdges
-            graph.addEdge(Arithmetic, Digit);
-            graph.addEdge(Arithmetic, Functions);
-            graph.addEdge(Arithmetic, OpenParenthesis);
-            graph.addEdge(Arithmetic, UnaryMinus);
-            graph.addEdge(Arithmetic, Decimal);
-
-            graph.addEdge(Digit, Arithmetic);
-            graph.addEdge(Digit, Digit);
-            graph.addEdge(Digit, Decimal);
-            graph.addEdge(Digit, Done);
-
+            /*
+                Add
+            */
+            graph.addEdge(Add, Double);
+            graph.addEdge(Add, Int);
+            graph.addEdge(Add, Functions);
+            graph.addEdge(Add, OpenParenthesis);
+            graph.addEdge(Add, UnaryMinus);
+            /*
+                Subtract
+            */
+            graph.addEdge(Subtract, Double);
+            graph.addEdge(Subtract, Int);
+            graph.addEdge(Subtract, Functions);
+            graph.addEdge(Subtract, OpenParenthesis);
+            graph.addEdge(Subtract, UnaryMinus);
+            /*
+                Multiply
+            */
+            graph.addEdge(Multiply, Double);
+            graph.addEdge(Multiply, Int);
+            graph.addEdge(Multiply, Functions);
+            graph.addEdge(Multiply, OpenParenthesis);
+            graph.addEdge(Multiply, UnaryMinus);
+            /*
+                Divide
+            */
+            graph.addEdge(Divide, Double);
+            graph.addEdge(Divide, Int);
+            graph.addEdge(Divide, Functions);
+            graph.addEdge(Divide, OpenParenthesis);
+            graph.addEdge(Divide, UnaryMinus);
+            /*
+                Int
+            */
+            graph.addEdge(Int, Add);
+            graph.addEdge(Int, Subtract);
+            graph.addEdge(Int, Multiply);
+            graph.addEdge(Int, Divide);
+            graph.addEdge(Int, Done);
+            graph.addEdge(Int, CloseParenthesis);
+            /*
+                Double
+            */
+            graph.addEdge(Double, Add);
+            graph.addEdge(Double, Subtract);
+            graph.addEdge(Double, Multiply);
+            graph.addEdge(Double, Divide);
+            graph.addEdge(Double, Done);
+            graph.addEdge(Double, CloseParenthesis);
+            /*
+                -
+            */
             graph.addEdge(UnaryMinus, UnaryMinus);
-            graph.addEdge(UnaryMinus, Digit);
+            graph.addEdge(UnaryMinus, Double);
+            graph.addEdge(UnaryMinus, Int);
             graph.addEdge(UnaryMinus, OpenParenthesis);
-            graph.addEdge(UnaryMinus, Decimal);
-
-            graph.addEdge(Functions, CloseParenthesis);
-            graph.addEdge(Functions, Digit);
-            graph.addEdge(Functions, Decimal);
+            /*
+                Functions
+            */
+            graph.addEdge(Functions, Double);
+            graph.addEdge(Functions, Int);
             graph.addEdge(Functions, UnaryMinus);
             graph.addEdge(Functions, Functions);
             graph.addEdge(Functions, OpenParenthesis);
-
-            graph.addEdge(OpenParenthesis, Digit);
-            graph.addEdge(OpenParenthesis, CloseParenthesis);
+            /*
+                (
+            */
+            graph.addEdge(OpenParenthesis, Double);
+            graph.addEdge(OpenParenthesis, Int);
             graph.addEdge(OpenParenthesis, Functions);
             graph.addEdge(OpenParenthesis, UnaryMinus);
-            graph.addEdge(OpenParenthesis, Decimal);
             graph.addEdge(OpenParenthesis, OpenParenthesis);
-
-            graph.addEdge(CloseParenthesis, Digit);
-            graph.addEdge(CloseParenthesis, Arithmetic);
+            /*
+                )
+            */
+            graph.addEdge(CloseParenthesis, Double);
+            graph.addEdge(CloseParenthesis, Int); ;
+            graph.addEdge(CloseParenthesis, Add);
+            graph.addEdge(CloseParenthesis, Subtract);
+            graph.addEdge(CloseParenthesis, Multiply);
+            graph.addEdge(CloseParenthesis, Divide);
             graph.addEdge(CloseParenthesis, Done);
-
-            graph.addEdge(Empty, Digit);
-            graph.addEdge(Empty, Decimal);
+            /*
+                Empty
+            */
+            graph.addEdge(Empty, Double);
+            graph.addEdge(Empty, Int);
             graph.addEdge(Empty, Functions);
             graph.addEdge(Empty, OpenParenthesis);
             graph.addEdge(Empty, UnaryMinus);
-            graph.addEdge(Empty, Done);
+            //graph.addEdge(Empty, Done);
 
-            graph.addEdge(Decimal, Digit);
             #endregion
             
+        }
+        private void evaluateRandom(List<string> equation) {
+            int numOpen = 0;
+            int numClose = 0;
+            string result = "";
+
+            foreach (string token in equation) {
+                Random random = new Random(Guid.NewGuid().GetHashCode());
+
+                switch (token) {
+                    case "Int":
+                        result += random.Next(9999);
+                        break;
+                    case "Double":
+                        double value = random.NextDouble() * 9999;
+                        value = System.Math.Round(value, random.Next(6));
+                        result += value;
+                        break;
+                    case "Functions":
+                        List<string> func = new List<string> { "sin(", "cos(", "tan(", "csc(", "log(", "ln("};
+                        result += func[random.Next(func.Count)];
+                        break;
+                    case "Unary Minus":
+                        result += "-";
+                        break;
+                    case "Done":
+                        break;
+                    default:
+                        result += token;
+                        break;
+                }
+            }
+
+            foreach (char ch in result) {
+                if (ch == '(')
+                {
+                    numOpen++;
+                }
+                else if (ch == ')') {
+                    numClose++;
+                }
+            }
+            while (true) {
+                if (numOpen > numClose)
+                {
+                    result += ")";
+                    numClose++;
+                }
+                break;
+            }
+            Console.WriteLine(result);
+
         }
     }
 }
--- CalculatorTests/Graph.cs
+++ CalculatorTests/Graph.cs
@@ -24,7 +24,13 @@ namespace CalculatorTests
             adjacentNodes.Add(node2);
             adjacent.Add(node1, adjacentNodes);
         }
-
+        public void removeEdge(Vertex node1, Vertex node2) {
+            // Update node1 values to not include node2
+            List<Vertex> adjacentNodes = adjacent[node1];
+            adjacent.Remove(node1);
+            adjacentNodes.Remove(node2);
+            adjacent.Add(node1, adjacentNodes);
+        }
         public override string ToString() {
             string output = "";
             foreach (KeyValuePair<Vertex, List<Vertex>> kvp in adjacent) {
@@ -37,11 +43,48 @@ namespace CalculatorTests
 
             return output;
         }
-        public List<string> randomTraversal() {
-
+        public List<string> randomTraversal(Vertex root) {
+            Random random = new Random();
             List<string> visited = new List<string>();
-            
-            return new List<string>();
+            Vertex currentNode = root;
+            string currentNodeValue = root.getLabel();
+            int totalOpen = 0;
+            int totalClose = 0;
+
+            while (!currentNode.getLabel().Equals("Done")) {
+                Vertex previousNode = currentNode;
+                int randomIndex = random.Next(adjacent[currentNode].Count);
+
+                // Traverse to new node
+                while (true) {
+                    currentNode = adjacent[previousNode][randomIndex];
+                    currentNodeValue = currentNode.getLabel();
+                    visited.Add(currentNodeValue);
+                    if (currentNode.getLabel().Equals(")")) // If the next node is a close parenthesis
+                    {
+                        if (totalOpen > totalClose) // make sure it is syntactically correct to add a close parenthesis
+                        {
+                            totalClose++;
+                            break;
+                        }
+                        else {
+                            continue;
+                        }
+                    }
+                    break;
+                }
+                
+                // Keep track of open parenthesis
+                switch (currentNodeValue) {
+                    case "(":
+                    case "Functions":
+                        totalOpen++;
+                        break;
+                }
+                
+            }
+
+            return visited;
         }
     }
 }
